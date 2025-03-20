ba95b3d7e5206c6a6094de27e8b5216b0b473eb9
jiienc <giannathan61@gmail.com>
2025-03-20 14:50:06 +0700

    first commit

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import router from './routes/index.js';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+// import Users from './models/userModel.js';
 
 const app = express();
 dotenv.config();
@@ -11,6 +12,7 @@ dotenv.config();
 try {
   await db.authenticate();
   console.log('Connection has been established successfully.');
+  // await Users.sync();
 } catch (error) {
   console.error('Unable to connect to the database:', error)
 }
